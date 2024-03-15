0c572bfff73106a7c8835b7e466e27e13f33d048
Trickster2001 <keyurmurkar@gmail.com>
2024-03-15 17:15:05 +0530

    completed

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Hello World");
+
+let cont = document.querySelector("#container");
+let love = document.querySelector("i");
+
+cont.addEventListener("dblclick", ()=>{
+  love.style.opacity = 0.8;
+  love.style.transform = "translate(-50%, -50%) scale(1)";
+  setTimeout(()=>{
+  love.style.opacity = 0;
+  // love.style.transform = "translate(-50%, -50%) scale(0)";
+  },1000) 
+})
